333c6605897f0e2709f92a86d69c083229eafbfe
MatthewSeat <125232821+MatthewSeat@users.noreply.github.com>
2023-02-12 21:59:20 -0500

    Add files via upload

---
+++ owoifier.js
@@ -0,0 +1,1 @@
+javascript:void function(){for(const e=document.body,o={l:"w",L:"W",r:"w",R:"W","\.":". :)"},n=document.createTreeWalker(e,NodeFilter.SHOW_TEXT);n.nextNode();){const c=n.currentNode;Object.keys(o).forEach(function(e){c.nodeValue=c.nodeValue.replace(new RegExp(e,"g"),o[e])})}}();
